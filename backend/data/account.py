3e9cbd8bfba6957288ad8fea63ee94c34979d804
Charlies-Kvit <theivangao@gmail.com>
2024-08-02 23:16:27 +0300

    изменил чуть api и конфиги

--- backend/data/account.py
+++ backend/data/account.py
@@ -4,14 +4,14 @@ from sqlalchemy import orm
 from sqlalchemy_serializer import SerializerMixin
 
 
-class User(SqlAlchemyBase, SerializerMixin):
-    __tablename__ = 'users'
+class Account(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'accounts'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.CHAR)
     email = sqlalchemy.Column(sqlalchemy.CHAR)
     avatar_url = sqlalchemy.Column(sqlalchemy.CHAR)
     group_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("groups.id"))
-    parse_results = orm.relationship("ParseResult", back_populates='user')
+    parse_results = orm.relationship("ParseResult", back_populates='account')
     group = orm.relationship('Groups')
 
--- backend/data/parse_resource.py
+++ backend/data/parse_resource.py
@@ -1,16 +1,36 @@
 from . import db_session
 import time
+import requests
 from flask_restful import abort, Resource, reqparse
 from .parse_result import ParseResult
-from flask import request
+from .account import Account
 from parsing.main_parse import main
 
-
 parser = reqparse.RequestParser()
-parser.add_argument("emails", action="append")
+parser.add_argument("emails", action="append", required=True)
+
+
+def abort_id_account_not_found(account_email):
+    db_sess = db_session.create_session()
+    group = db_sess.query(ParseResult).filter(ParseResult.account_email == account_email).first()
+    db_sess.close()
+    if not group:
+        abort(404, message=f"Account {account_email} not found")
 
 
 class ParseRequestResource(Resource):
+    def get(self, account_email):
+        abort_id_account_not_found(account_email)
+        db_sess = db_session.create_session()
+        parse_res = db_sess.query(ParseResult).filter(ParseResult.account_email == account_email).first()
+        db_sess.close()
+        return parse_res.to_dict(only=("id", "title", "url", "account_post_count", "text", "account_id"))
+
+    def put(self, account_email):
+        pass
+
+
+class ParseRequestListResource(Resource):
     def post(self):
         args = parser.parse_args()
         emails = args["emails"]
@@ -20,7 +40,7 @@ class ParseRequestResource(Resource):
         do_emails = []
         db_sess = db_session.create_session()
         for email in emails:
-            parse_res = db_sess.query(ParseResult).filter(ParseResult.user_email == email).first()
+            parse_res = db_sess.query(ParseResult).filter(ParseResult.account_email == email).first()
             if not parse_res:
                 get_emails.append(email)
                 continue
@@ -28,22 +48,18 @@ class ParseRequestResource(Resource):
         result = main(get_emails)
         for email in get_emails:
             for el in result[email]:
+                account = db_sess.query(Account).filter(Account.email == email).first()
                 parse_result = ParseResult(
                     text=el["text"],
                     title=el["title"],
                     timestamp=int(el["timestamp"]),
                     last_parsing_time=time.time(),
-                    user_email=email,
-                    user_post_count=el["count"],
-                    url=el["url"]
+                    account_email=email,
+                    account_post_count=el["count"],
+                    url=el["url"],
+                    account_id=account.id
                 )
                 db_sess.add(parse_result)
                 db_sess.commit()
-        answer = {}
-        for email in emails:
-            email_parse_res = db_sess.query(ParseResult).filter(
-                ParseResult.user_email == email
-            ).all()
-            answer[email] = [parse_res.to_dict(only=("id", "title", "url", "user_post_count", "text"))
-                             for parse_res in email_parse_res]
-        return answer
+                db_sess.close()
+        return {"success": "OK"}
