8f0905eb3de1dc240061e967ef1680c4c05ca8f4
KDmasterLOL <ivantolmachevmaster@gmail.com>
2024-11-09 15:54:00 +0300

    Change something and change favicon

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,30 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://49.13.48.163/', // on vm, set to ip adress of vm
+      "/api": {
+        target: "http://49.13.48.163/", // on vm, set to ip adress of vm
         changeOrigin: true,
         secure: false,
         configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
           });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Sending Request to the Target:", req.method, req.url);
           });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url,
+            );
           });
         },
-      }
-    }
-  }
-
+      },
+    },
+  },
 });
